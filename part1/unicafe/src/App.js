469eea6e671826d5dd1711b19f2b8a5cd26356d6
Sashini Hettiarachchi <sashini.hettiarachchi1@gmail.com>
2023-05-25 00:11:12 +0530

    1.11  add table

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,10 +1,11 @@
 import { useState } from "react";
+
 import Header from "./components/Header";
 import Button from "./components/Button";
 import Statistics from "./components/Statistics";
+import './App.css'
 
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
--- part1/unicafe/src/components/Statistics.js
+++ part1/unicafe/src/components/Statistics.js
@@ -7,15 +7,43 @@ const Statistics = (props) => {
   const avg = (good * 1 + neutral * 0 + bad * -1) / total || 0;
   const positivePer = (good / total) * 100 || 0;
   return (
-    <p>
+    <>
       <Header title={"statistics"} />
-      <StatisticsLine text={"good"} count={good} />
+      <table className="statTable">
+        <tbody>
+          <tr>
+            <td>{"good"}</td>
+            <td>{good}</td>
+          </tr>
+          <tr>
+            <td>{"neutral"}</td>
+            <td>{neutral}</td>
+          </tr>
+          <tr>
+            <td>{"bad"}</td>
+            <td>{bad}</td>
+          </tr>
+          <tr>
+            <td>{"all"}</td>
+            <td>{total}</td>
+          </tr>
+          <tr>
+            <td>{"average"}</td>
+            <td>{avg}</td>
+          </tr>
+          <tr>
+            <td>{"positive"}</td>
+            <td>{`${positivePer}%`}</td>
+          </tr>
+        </tbody>
+      </table>
+      {/* <StatisticsLine text={"good"} count={good} />
       <StatisticsLine text={"neutral"} count={neutral} />
       <StatisticsLine text={"bad"} count={bad} />
       <StatisticsLine text={"all"} count={total} />
       <StatisticsLine text={"average"} count={avg} />
-      <StatisticsLine text={"positive"} count={`${positivePer}%`} />
-    </p>
+      <StatisticsLine text={"positive"} count={`${positivePer}%`} /> */}
+    </>
   );
 };
 
