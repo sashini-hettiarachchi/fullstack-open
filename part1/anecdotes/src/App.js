930a48a628378ad0f2365e9ecbe2d45d0a010964
Sashini Hettiarachchi <sashini.hettiarachchi1@gmail.com>
2023-05-25 18:19:58 +0530

    1.14 display the anecdote with the largest number of votes

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -14,6 +14,7 @@ const App = () => {
 
   const [selected, setSelected] = useState(0);
   const [points, setPoints] = useState([]);
+  const [maxPointIndex, setMaxPointIndex] = useState(0);
 
   const moveToNextAnecdote = () => {
     const randomIndex = Math.floor(Math.random() * (8 - 1) + 1);
@@ -21,19 +22,43 @@ const App = () => {
   };
 
   const voteForAnecdote = () => {
-    const dupPointsArray = [...points];
-    dupPointsArray[selected] = points[selected] ? points[selected] + 1 : 1;
-    setPoints(dupPointsArray);
+    const tempPointsArray = [...points];
+    tempPointsArray[selected] = points[selected] ? points[selected] + 1 : 1;
+    setPoints(tempPointsArray);
+    const maxIndex = getGreatestNumberIndex(tempPointsArray);
+    setMaxPointIndex(maxIndex);
   };
-  console.log("points", points);
+
+  const getGreatestNumberIndex = (arr) => {
+    if (!arr.length) return -1;
+    let max = arr[0];
+    let maxIndex = 0;
+
+    for (let i = 1; i < arr.length; i++) {
+      if (arr[i] > max) {
+        maxIndex = i;
+        max = arr[i];
+      }
+    }
+    return maxIndex;
+  };
+
   return (
     <div>
       <h3>{anecdotes[selected]}</h3>
       <h3>has {points[selected] || 0} votes</h3>
       <div>
         <button onClick={voteForAnecdote}>vote</button>
-        <button onClick={moveToNextAnecdote}>nex anecdote</button>
+        <button onClick={moveToNextAnecdote}>next anecdote</button>
       </div>
+      {points.length ? (
+        <>
+          <h3>Anecdotes with most votes</h3>
+          <p>{anecdotes[maxPointIndex]}</p>
+        </>
+      ) : (
+        <p>No votes yet</p>
+      )}
     </div>
   );
 };
